1e59ee2a2f52436a295b4999611cc877dcccd385
blesswrld <roninfortnite71@gmail.com>
2025-05-02 23:46:16 +0300

    switch cases, couple more examples with type narrawing and type guard

--- index.ts
+++ index.ts
@@ -27,35 +27,69 @@ function isNumber(n: unknown): n is number {
 // Интерфейсы
 
 interface Car {
+    name: "car";
     engine: string;
-    wheels: {
-        number: number;
-        string: string;
-    };
+    wheels: number;
 }
 
 interface Ship {
+    name: "ship";
     engine: string;
     sail: string;
 }
 
-function repairVehicle(vehicle: Car | Ship): void {
+interface Airplane {
+    name: "airplane";
+    engine: string;
+    wings: string;
+}
+
+interface SuperAirplane {
+    name: "SuperAirplane";
+    engine: string;
+    wings: string;
+}
+
+type Vehicle = Car | Ship | Airplane | SuperAirplane;
+
+function repairVehicle(vehicle: Vehicle): void {
     // Условия
-    if (isCar(vehicle)) {
-        vehicle.wheels;
-    } else if (isShip(vehicle)) {
-        vehicle;
-    } else {
-        vehicle; // never
+    // if (isCar(vehicle)) {
+    //     vehicle.wheels;
+    // } else if (isShip(vehicle)) {
+    //     vehicle;
+    // } else {
+    //     vehicle.wings;
+    // }
+
+    // type guard with narrawing types
+    // --- Switch Cases ---
+    switch (vehicle.name) {
+        case "car":
+            console.log(vehicle.wheels);
+            break;
+        case "ship":
+            console.log(vehicle.sail);
+            break;
+        case "airplane":
+            console.log(vehicle.wings);
+            break;
+        case "SuperAirplane":
+            console.log(vehicle.wings);
+            break;
+        default:
+            const smth: never = vehicle; // never
+            console.log("Opps error!");
+            break;
     }
 }
 
-// Функция Type-Guard
-function isCar(car: Car | Ship): car is Car {
-    return (car as Car).wheels.number !== undefined; // Утверждаем тип автомобиля с определенными параметрам
-}
+// // Функция Type-Guard
+// function isCar(car: Vehicle): car is Car {
+//     return (car as Car).wheels.number !== undefined; // Утверждаем тип автомобиля с определенными параметрам
+// }
 
-// Еще функция Type-Guard
-function isShip(ship: Car | Ship): ship is Ship {
-    return "sail" in ship; // Утверждаем тип автомобиля с определенными параметрам
-}
+// // Еще функция Type-Guard
+// function isShip(ship: Car | Ship | Airplane): ship is Ship {
+//     return "sail" in ship; // Утверждаем тип автомобиля с определенными параметрам
+// }
